754eca1445e323ff7007ced7ddafee68ac168bca
Yko-koshka <legezaanna7@gmail.com>
2022-07-12 19:42:43 +0200

    Created components: Results, Meaning

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
 function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [results, setResult] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResult(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`${keyword}`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
@@ -21,9 +23,12 @@ function Dictionary() {
     setKeyword(event.target.value);
   }
   return (
-    <form className="Dictionary" onSubmit={search}>
-      <input type="search" onChange={handleKeywordChange} />
-    </form>
+    <div className="Dictionary">
+      <form className="Dictionary" onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
   );
 }
 
