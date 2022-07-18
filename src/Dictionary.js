92239798a96fa78fb270d8a1d4906fabf503c926
Yko-koshka <legezaanna7@gmail.com>
2022-07-18 23:30:12 +0200

    Added api pictures

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
 import Results from "./Results";
 import axios from "axios";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000012f8a85872a184e1e9052121f0a09c431";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios
+      .get(pexelsApiUrl, {
+        headers: headers,
+      })
+      .then(handlePexelsResponse);
   }
 
   function handelSubmit(event) {
@@ -51,6 +67,7 @@ function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Photos.css";
+
+function Photos(props) {
+  if (props.photos) {
+    console.log(props.photos);
+    return (
+      <section className="Photos">
+        {props.photos.map(function (photo, index) {
+          return (
+            <div key={index}>
+              <a href={photo.src.original}>
+                <img src={photo.src.tiny} alt="" className="img-fluid" />
+              </a>
+            </div>
+          );
+        })}
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
+
+export default Photos;
